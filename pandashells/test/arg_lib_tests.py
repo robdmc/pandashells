43d537c2710deb15809a568f6d5c56c5155fd9ce
Rob deCarvalho <robdmc@gmail.com>
2014-11-09 10:03:06 -0500

    added file to run arg_lib_tests (forgot to do this in last commit)

---
+++ pandashells/test/arg_lib_tests.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python
+import os
+import json
+from unittest import TestCase
+from pandashells.lib import arg_lib
+import argparse
+
+class ArgLibTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_check_for_recognized_args_bad_args(self):
+        """
+        _check_for_recognized_args properly recognizes bad arguments
+        """
+        with self.assertRaises(ValueError):
+            arg_lib._check_for_recognized_args('unrecognized', 'args')
+
+    def test_check_for_recognized_args_good_args(self):
+        """
+        _check_for_recognized_args properly accepts good args
+        """
+        args = [
+            'io_in',
+            'io_out',
+            'example',
+            'xy_plotting',
+            'decorating',
+        ]
+        arg_lib._check_for_recognized_args(*args)
+
+    def test_io_in_adder_bad_args(self):
+        """
+        _io_in_adder properly recognizes bad arguments
+        """
+        parser = argparse.ArgumentParser()
+        arg_lib._io_in_adder
+
+
+
+#class GlobalArgTests(unittest.TestCase):
+#    def test_home_path_looks_right(self):
+#        """
+#        The path to the users home directory looks right
+#        """
+#        home = os.path.expanduser('~')
+#        self.assertEqual(config_lib.HOME, home)
+#
+#    def test_default_opt_dict_exists(self):
+#        """
+#        The dictionary of default options exists
+#        """
+#        self.assertTrue(len(config_lib.DEFAULT_DICT) > 0)
+#
+#
+#
+#class GetConfigTests(unittest.TestCase):
+#    def setUp(self):
+#        #self.orig_file_name = config_lib.CONFIG_FILE_NAME
+#        if os.path.isfile(config_lib.CONFIG_FILE_NAME):
+#            os.system('cp {f} {f}_orig'.format(f=config_lib.CONFIG_FILE_NAME))
+#
+#    def tearDown(self):
+#        if os.path.isfile(config_lib.CONFIG_FILE_NAME + '_orig'):
+#            os.system('mv {f}_orig {f}'.format(f=config_lib.CONFIG_FILE_NAME))
+#        else:
+#            os.system('rm  {f}'.format(f=config_lib.CONFIG_FILE_NAME))
+#
+#    def test_set_config_creates_file(self):
+#        """
+#        set_config() function writes to file
+#        """
+#        expected_dict = {'name': 'John'}
+#        config_lib.set_config(expected_dict)
+#        saved_dict = json.loads(open(config_lib.CONFIG_FILE_NAME).read())
+#        self.assertEqual(expected_dict, saved_dict)
+#
+#    def test_get_config_non_existent_file(self):
+#        """
+#        get_config() creates config file when it doesn't exist
+#        """
+#        if os.path.isfile(config_lib.CONFIG_FILE_NAME):
+#            os.system('rm {}'.format(config_lib.CONFIG_FILE_NAME))
+#        config = config_lib.get_config()
+#        self.assertEqual(config_lib.DEFAULT_DICT, config)
+#
+#    def test_get_config_existing_file(self):
+#        """
+#        get_config() reads existing file
+#        """
+#        if os.path.isfile(config_lib.CONFIG_FILE_NAME):
+#            os.system('rm {}'.format(config_lib.CONFIG_FILE_NAME))
+#
+#        test_config = {'name': 'Bill'}
+#        with open(config_lib.CONFIG_FILE_NAME, 'w') as f:
+#            f.write(json.dumps(test_config))
+#        config = config_lib.get_config()
+#        self.assertEqual(test_config, config)
