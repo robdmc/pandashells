6ff41047b76cd1976c18c0169c1f1581f97e802a
Rob deCarvalho <robdmc@gmail.com>
2015-02-01 14:21:45 -0500

    need to write tests for p.sig_edit executable

--- pandashells/bin/p_sig_edit.py
+++ pandashells/bin/p_sig_edit.py
@@ -6,48 +6,15 @@ import sys
 import argparse
 import re
 
-from pandashells.lib import module_checker_lib, arg_lib, io_lib
+from pandashells.lib import module_checker_lib, arg_lib, io_lib, outlier_lib
 
 # import required dependencies
-modulesOkay = module_checker_lib.check_for_modules(['pandas', 'numpy'])
-if not modulesOkay:
-    sys.exit(1)
+module_checker_lib.check_for_modules(['pandas', 'numpy'])
 
 import pandas as pd
 import numpy as np
 
 
-# recursive edit a series
-def sigma_edit_series(sigma_thresh, in_series, ref_series=None):
-    if in_series.count() == 0:
-        msg = "Error:  All values edited out."
-        raise ValueError(msg)
-
-    ref = ref_series if ref_series else in_series.mean()
-    resid = in_series - ref
-    std = resid.std()
-    sigma_t = sigma_thresh * std
-    outside = resid.abs() >= sigma_t
-    if any(outside):
-        in_series[outside] = np.NaN
-        in_series = sigma_edit_series(sigma_thresh, in_series, ref_series)
-
-    return in_series
-
-def ensure_col_exists(df, col, df_name):
-    if df and (col not in df.columns):
-        msg = '\n\n{} does not have column {}\n\n'.format(df_name, col)
-        raise ValueError(msg)
-
-
-def sigma_edit_dataframe(sigma_thresh, columns, df, df_ref=None):
-    for col in columns:
-        ensure_col_exists(df, col, 'df')
-        ensure_col_exists(df_ref, col, 'df_ref')
-        ser = df[col_name]
-        ref_ser = df_ref[col_name] if df_ref else None
-        df[col] = sigma_edit_series(sigma_thresh, ser, ref_series=ref_ser)
-    return df
 
 
 def main()
@@ -65,6 +32,8 @@ def main()
                         nargs=1, required=True, type=float)
     parser.add_argument("-c", "--cols", required=True,
                         help="Column(s) to sigma-edit", nargs="+")
+    parser.add_argument("--max_iter", help="Max number of recursions",
+                        nargs=1,  type=int, default=[20])
 
     # parse arguments
     args = parser.parse_args()
@@ -72,29 +41,8 @@ def main()
     # get the input dataframe
     df = io_lib.df_from_input(args)
 
-    # define function to perform one sigma-edit pass
-    def single_pass(in_series, sigma_thresh):
-        if in_series.count() == 0:
-            msg = "Error:  All values edited out.  Try "
-        needs_another_pass = False
-        mu = in_series.mean()
-        resid = in_series - mu
-        std = in_series.std()
-        sigma_t = sigma_thresh * std
-        outside = resid.abs() > sigma_t
-
-        if sum(outside) > 0:
-            needs_another_pass = True
-        in_series[outside] = np.NaN
-        return needs_another_pass, in_series
-
-    # sigma-edit each column
-    for col_name in args.cols:
-        needs_another_pass = True
-        ser = df[col_name]
-        while needs_another_pass:
-            needs_another_pass, ser = single_pass(ser, args.sigma_thresh[0])
-        df[col_name] = ser
+    df = outlier_lib.sigma_edit_dataframe(
+        args.sigma_thresh[0], args.cols, df, max_iter=args.max_iter[0])
 
     # write dataframe to output
     io_lib.df_to_output(args, df)
