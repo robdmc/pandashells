d37ad0ff70a490c1e6e7661185d32893418f26bc
Rob deCarvalho <robdmc@gmail.com>
2015-03-02 06:54:38 -0500

    Got p.plot tests passing at 100% coverage
    Squashed commit of the following:
    
    commit 3e7a1b9ece02a3a86d83ff762dacaea64a2e8f78
    Author: Rob deCarvalho <robdmc@gmail.com>
    Date:   Mon Mar 2 06:53:05 2015 -0500
    
        finished p.plot tests
    
    commit ec6cde6c50c135e637e8a20e31e6fd90956f2b74
    Author: Rob deCarvalho <robdmc@gmail.com>
    Date:   Mon Mar 2 06:05:05 2015 -0500
    
        got set up for writing p_plot tests

---
+++ pandashells/bin/p_plot.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import argparse
+
+from pandashells.lib import module_checker_lib, arg_lib, io_lib, plot_lib
+
+module_checker_lib.check_for_modules(['pandas', 'matplotlib'])
+
+
+def main():
+    #TODO: Finish this help message
+    msg = "Write "
+    msg += "something here"
+
+    # read command line arguments
+    parser = argparse.ArgumentParser(description=msg)
+
+    arg_lib.add_args(parser, 'io_in', 'xy_plotting', 'decorating', 'example')
+
+    #TODO: Move this to the decorating function
+    parser.add_argument("-a", "--alpha", help="Set opacity",  # WHY ISN'T THIS IN DECORATING?
+                        nargs=1, default=[1.], type=float)
+
+    # parse arguments
+    args = parser.parse_args()
+
+    # get the input dataframe
+    df = io_lib.df_from_input(args)
+
+    # set the appropriate theme
+    plot_lib.set_plot_styling(args)
+
+    # draw the plot
+    plot_lib.draw_xy_plot(args, df)
+
+
+if __name__ == '__main__':  # pragma: nocover
+    main()
---
+++ pandashells/test/p_plot_test.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+from mock import patch, MagicMock
+from unittest import TestCase
+
+from pandashells.bin.p_plot import main
+
+
+class MainTests(TestCase):
+    @patch('pandashells.bin.p_plot.argparse.ArgumentParser')
+    @patch('pandashells.bin.p_plot.arg_lib.add_args')
+    @patch('pandashells.bin.p_plot.io_lib.df_from_input')
+    @patch('pandashells.bin.p_plot.plot_lib.set_plot_styling')
+    @patch('pandashells.bin.p_plot.plot_lib.draw_xy_plot')
+    def test_plotting(
+            self, draw_xy_mock, set_plot_styling_mock, df_from_input_mock,
+            add_args_mock, ArgumentParserMock):
+        args = MagicMock()
+        parser = MagicMock(parse_args=MagicMock(return_value=args))
+        ArgumentParserMock.return_value = parser
+        df_from_input_mock.return_value = 'df'
+
+        main()
+
+        add_args_mock.assert_called_with(
+            parser, 'io_in', 'xy_plotting', 'decorating', 'example')
+
+        df_from_input_mock.assert_called_with(args)
+        set_plot_styling_mock.assert_called_with(args)
+        draw_xy_mock.assert_called_with(args, 'df')
