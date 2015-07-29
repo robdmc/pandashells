6d6699a38e0cdf5cf3f3b59c21e9e8e8e7c0a2c0
Rob deCarvalho <robdmc@gmail.com>
2015-07-29 16:56:51 -0400

    fixed import problems in __init__.py

--- pandashells/__init__.py
+++ pandashells/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa
 
-from pandashells.outlier_lib import sigma_edit_dataframe as sigma_edit
-from pandashells.outlier_lib import lomb_scargle
+from pandashells.lib.outlier_lib import sigma_edit_dataframe as sigma_edit
+from pandashells.lib.lomb_scargle_lib import lomb_scargle
