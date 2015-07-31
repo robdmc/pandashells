6a3a2959aed0cb8f00da8796bb032fbcb6da6a5a
Rob deCarvalho <robdmc@gmail.com>
2015-07-30 20:14:15 -0400

    modified some examples

--- examples/config.examples.sh
+++ examples/config.examples.sh
@@ -2,7 +2,7 @@
 
 # p.config 
 p.config > /dev/null
-p.config --force_defaults > /dev/null
 p.config --io_input_header noheader --io_input_type table > /dev/null
 p.config --io_output_header noheader --io_output_type table > /dev/null
+p.config --force_defaults > /dev/null
 
--- examples/crypt.examples.sh
+++ examples/crypt.examples.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
 #p.crypt
-echo 'plain text' > file.txt && p.crypt -i file.txt -v -o file.txt.crypt && p.crypt -d -i file.txt.crypt -o file_restored.txt && rm file.txt*
+echo 'plain text' > file.txt && p.crypt -i file.txt -v -o file.txt.crypt
+p.crypt -d -i file.txt.crypt -o file_restored.txt 
+echo Original
+cat file.txt
+echo
+echo Final
+cat file_restored.txt
+rm file*
 
