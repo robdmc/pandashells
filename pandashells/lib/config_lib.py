2591744d6d28a8a99b6821b3723b228cb5a274f4
Rob deCarvalho <robdmc@gmail.com>
2015-08-01 11:21:34 -0400

    bug fix

--- pandashells/lib/config_lib.py
+++ pandashells/lib/config_lib.py
@@ -17,7 +17,7 @@ CONFIG_OPTS = sorted(
         ('plot_context', ['talk', 'poster', 'paper', 'notebook']),
         ('plot_theme', ['darkgrid', 'whitegrid', 'dark', 'white']),
         ('plot_palette', ['muted', 'deep', 'dark', 'colorblind', 'pastel']),
-        ('plot_backend', ['TkAgg', 'MacOSX', 'Qt4Agg']),
+        ('plot_backend', ['TkAgg', 'MacOSX', 'Qt4Agg', 'Agg']),
 
     ]
 )
